c03fd28be0abfeeba33d7338e007d72926154f88
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-01-09 10:44:40 -0300

    feat: add CLI logic without error handling

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         ApiConfig apiConfig = new ApiConfig();
 
-        apiConfig.setRequest("Leturnos");
+        if (args.length == 0) {
+            System.out.println(
+                    "Welcome to GitHub User Activity, " +
+                            "please insert 'github-activity <username>' to use");
+        } else {
+            if (args[0].equals("github-activity")) {
+                apiConfig.setRequest(args[1]);
+            } else {
+                System.out.println("please insert 'github-activity' before the username");
+            }
+        }
     }
 }
