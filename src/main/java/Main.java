2e0794111e65acc6bf13aadc5823e5f975622ea8
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-01-14 15:11:10 -0300

    feat: refactor main entry point to support CLI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,18 @@ import java.net.http.HttpResponse;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
+        if (args.length != 1) {
             System.out.println(
                     "Welcome to GitHub User Activity, " +
                             "please insert 'github-activity <username>' to use");
         } else {
-            if (args[0].equals("github-activity") && args.length == 2) {
-                try {
-                    HttpResponse<String> response = ApiConfig.setRequest(args[1]);
-                    CliFormatter.printFormattedResponse(response);
-                } catch (RuntimeException e) {
-                    System.out.println("Error: " + e.getMessage());
-                } catch (Exception e) {
-                    System.out.println("Unexpected error occurred.");
-                }
-            } else {
-                System.out.println("please insert 'github-activity <username>' to use");
+            try {
+                HttpResponse<String> response = ApiConfig.setRequest(args[0]);
+                CliFormatter.printFormattedResponse(response);
+            } catch (RuntimeException e) {
+                System.out.println("Error: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Unexpected error occurred.");
             }
         }
     }
